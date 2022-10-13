eb4dc6b7a3daeabbca1e4749261e74cef0c09b1f
mucke12 <65766946+mucke12@users.noreply.github.com>
2022-10-13 23:09:41 +0200

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ module.exports = {
     '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', ''],
   webpackFinal: async (config) => {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
     return config;
